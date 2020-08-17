677060649c8e7126fe94e614943c4a9dc37bea02
ALIENK9 <alezanga@outlook.it>
2020-08-17 17:26:48 +0200

    Uml diagrams, requirements version, deleted files

--- data_preprocessor/data/CsvSaver.py
+++
@@ -1,6 +0,0 @@
-from data_preprocessor.data import Saver
-
-
-class CsvSaver(Saver):
-    def write(self, df, **kwargs):
-        df._Frame__df.to_csv(**kwargs)
--- data_preprocessor/data/__init__.py
+++ data_preprocessor/data/__init__.py
@@ -1,5 +1,3 @@
-from data_preprocessor.data.loaders import Loader
-from data_preprocessor.data.Saver import Saver
 from data_preprocessor.data.Frame import Frame
-from data_preprocessor.data.CsvSaver import CsvSaver
 from data_preprocessor.data.Shape import Shape
+from data_preprocessor.data.loaders import Loader
