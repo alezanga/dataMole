62ee9cbef362699ca71d70accdca73b70832349f
alek9z <alezanga@outlook.it>
2020-11-18 15:07:56 +0100

    Working scatterplot

--- dataMole/gui/charts/scatterplot.py
+++ dataMole/gui/charts/scatterplot.py
@@ -21,21 +21,24 @@
 # You should have received a copy of the GNU General Public License
 # along with DataMole.  If not, see <https://www.gnu.org/licenses/>.
 
-from typing import List, Optional, Union, Set
+from typing import List, Optional, Union, Set, Tuple, Any
 
 import numpy as np
 import pandas as pd
 import pyqtgraph as pg
-from PySide2.QtCore import Slot, Qt, QModelIndex
+from PySide2.QtCore import Slot, Qt, QModelIndex, QThreadPool, QObject, Signal
 from PySide2.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout, QPushButton, \
-    QComboBox, QSplitter, QSizePolicy
+    QComboBox, QSplitter, QSizePolicy, QApplication, QCheckBox
 
 from dataMole.data.types import Types
 from dataMole.gui.charts.utils import randomColors
 from dataMole.gui.mainmodels import SearchableAttributeTableWidget, FrameModel, \
     AttributeTableModel, AttributeProxyModel
 from dataMole.gui.panels.dataview import DataView
+from dataMole.gui.widgets.waitingspinnerwidget import QtWaitingSpinner
 from dataMole.gui.workbench import WorkbenchModel
+from dataMole.threads import Worker
+from dataMole.utils import safeDelete
 
 
 class ScatterPlotMatrix(DataView):
@@ -50,14 +53,28 @@ class ScatterPlotMatrix(DataView):
                                                                  [Types.Numeric, Types.Ordinal])
         matrixLabel = QLabel('Select at least two numeric attributes and press \'Create chart\' to plot')
         matrixLabel.setWordWrap(True)
-        createButton = QPushButton('Create chart', self)
+        self.__createButton = QPushButton('Create chart', self)
         self.__colorByBox = QComboBox(self)
+        self.__autoDownsample = QCheckBox('Auto downsample', self)
+        self.__useOpenGL = QCheckBox('Use OpenGL', self)
+        self.__autoDownsample.setToolTip('If too many points are to be rendered, this will try\n'
+                                         'to plot only a subsample, improving performance with\n'
+                                         'zooming and panning, but increasing rendering time')
+        self.__useOpenGL.setToolTip('Enforce usage of GPU acceleration to render charts.\n'
+                                    'It is still an experimental feature but should speed\n'
+                                    'up rendering with huge set of points')
+
+        # Layout for checkboxes
+        optionsLayout = QHBoxLayout()
+        optionsLayout.addWidget(self.__autoDownsample, 0, Qt.AlignRight)
+        optionsLayout.addWidget(self.__useOpenGL, 0, Qt.AlignRight)
 
         sideLayout.addWidget(matrixLabel)
         sideLayout.addWidget(self.__matrixAttributes)
+        sideLayout.addLayout(optionsLayout)
         sideLayout.addWidget(self.__colorByBox, 0, Qt.AlignBottom)
-        sideLayout.addWidget(createButton, 0, Qt.AlignBottom)
-        self.__matrixLayout: pg.GraphicsLayoutWidget = pg.GraphicsLayoutWidget(self)
+        sideLayout.addWidget(self.__createButton, 0, Qt.AlignBottom)
+        self.__matrixLayout: pg.GraphicsLayoutWidget = pg.GraphicsLayoutWidget()
         self.__layout = QHBoxLayout(self)
         self.__comboModel = AttributeProxyModel([Types.String, Types.Ordinal, Types.Nominal], self)
 
@@ -77,71 +94,13 @@ class ScatterPlotMatrix(DataView):
         self.__layout.addWidget(self.__splitter)
         self.__splitter.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         # Connect
-        createButton.clicked.connect(self.showScatterPlots)
+        self.__createButton.clicked.connect(self.showScatterPlots)
 
-    @staticmethod
-    def __processCategoricalColumn(df: pd.DataFrame) -> pd.DataFrame:
-        """ For every categorical column in 'df' replace categories with their numerical code,
-        propagating Nan values. Categorical columns are converted to float """
-        categoricalColumns = df.select_dtypes(include='category').columns.to_list()
-        if categoricalColumns:
-            df = df.copy(True)
-            df[categoricalColumns] = \
-                df[categoricalColumns].apply(lambda c: c.cat.codes, axis=0).replace(-1, np.nan)
-        return df
-
-    @staticmethod
-    def __createScatterSeries(df: Union[pd.DataFrame, pd.core.groupby.DataFrameGroupBy], xCol: str,
-                              yCol: str, groupBy: bool) -> List[pg.PlotDataItem]:
-        allSeriesPlain = list()
-        if groupBy:
-            df: pd.core.groupby.DataFrameGroupBy
-            n = len(df)
-            colours = randomColors(n)
-            i = 0
-            for groupName, groupedDf in df:
-                gdf = groupedDf.dropna()
-                qSeries1 = pg.PlotDataItem(x=gdf[xCol], y=gdf[yCol], autoDownsample=True,
-                                           name=str(groupName), symbolBrush=pg.mkBrush(colours[i]),
-                                           symbol='o', pen=None)
-                allSeriesPlain.append(qSeries1)
-                i += 1
-        else:
-            df: pd.DataFrame
-            df = df.dropna()
-            series = pg.PlotDataItem(x=df[xCol], y=df[yCol], autoDownsample=True, symbol='o', pen=None)
-            allSeriesPlain.append(series)
-        return allSeriesPlain
-
-    # @staticmethod
-    # def __setupChartFromSeries(seriesList: List[pg.PlotDataItem], xAxisName: str,
-    #                            yAxisName: str) -> QtCharts.QChart:
-    #     chart = pg.PlotDataItem()
-    #     for series in seriesList:
-    #         series.setMarkerSize(8)
-    #         series.setUseOpenGL(True)
-    #         chart.addSeries(series)
-    #     # chart.setDropShadowEnabled(False)
-    #     chart.legend().setVisible(False)
-    #     chart.createDefaultAxes()
-    #     # Add axes names but hide them
-    #     chart.axisX().setTitleText(xAxisName)
-    #     chart.axisY().setTitleText(yAxisName)
-    #     chart.axisX().setTitleVisible(False)
-    #     chart.axisY().setTitleVisible(False)
-    #     # Set font size for axis
-    #     font: QFont = chart.axisX().labelsFont()
-    #     font.setPointSize(9)
-    #     chart.axisX().setLabelsFont(font)
-    #     chart.axisY().setLabelsFont(font)
-    #     chart.setMargins(QMargins(5, 5, 5, 5))
-    #     chart.layout().setContentsMargins(2, 2, 2, 2)
-    #     return chart
+        self.spinner = QtWaitingSpinner(self.__matrixLayout)
 
     @Slot()
     def showScatterPlots(self) -> None:
-        # Clear eventual existing plots
-        self.clearScatterPlotMatrix()
+        self.__createButton.setDisabled(True)
         # Create plot with selected attributes
         attributes: Set[int] = self.__matrixAttributes.model().checked
         if len(attributes) < 2:
@@ -159,68 +118,94 @@ class ScatterPlotMatrix(DataView):
                 self.__comboModel.index(selectedIndex, 0, QModelIndex()))
             group = index.row() if index.isValid() else None
 
+        # Create a new matrix layout and delete the old one
+        matrix = pg.GraphicsLayoutWidget(parent=self)
+        self.spinner = QtWaitingSpinner(matrix)
+        oldM = self.__splitter.replaceWidget(0, matrix)
+        self.__matrixLayout = matrix
+        safeDelete(oldM)
+        matrix.useOpenGL(self.__useOpenGL.isChecked())
+        matrix.show()
+
         # Get attributes of interest
         toKeep: List[int] = list(attributes) if group is None else [group, *attributes]
         filterDf = self.__frameModel.frame.getRawFrame().iloc[:, toKeep]
-        groupName: Optional[str] = filterDf.columns[0] if group is not None else None
-        # Convert categories to numeric, but exclude groupBy attributes since categories are needed there
-        processed = self.__processCategoricalColumn(filterDf.iloc[:, 1:] if groupName else filterDf)
-        # Save attribute names for later use. Groupby column name is purposely excluded
-        attributesColumnNames: List[str] = processed.columns.to_list()
-        if groupName:
-            df = pd.concat([filterDf.loc[:, groupName], processed], axis=1, ignore_index=False)
-            # Group by selected attribute, if present
-            df = df.groupby(groupName)
-        else:
-            df = filterDf
+        # Create a worker to create scatter-plots on different thread
+        worker = Worker(ProcessDataframe(), (filterDf, group, attributes))
+
+        worker.signals.result.connect(self.__createPlots)
+        # No need to deal with error/finished signals since there is nothing to do
+        worker.setAutoDelete(True)
+        self.spinner.start()
+        QThreadPool.globalInstance().start(worker)
+
+    def resetScatterPlotMatrix(self) -> None:
+        # Create a new matrix layout
+        matrix = pg.GraphicsLayoutWidget(parent=self)
+        self.spinner = QtWaitingSpinner(matrix)
+        oldM = self.__splitter.replaceWidget(0, matrix)
+        self.__matrixLayout = matrix
+        safeDelete(oldM)
+        matrix.show()
+
+    @Slot(object, object)
+    def __createPlots(self, _, result: Tuple[pd.DataFrame, List[str], List[int], bool]) -> None:
+        """ Create plots and render all graphic items """
+        # Unpack results
+        df, names, attributes, grouped = result
 
         # Populate the matrix
         for r in range(len(attributes)):
-            # self.__matrixLayout.setRowStretch(r, 1)
-            # self.__matrixLayout.setColumnStretch(r, 1)
             for c in range(len(attributes)):
                 if r == c:
-                    name: str = attributesColumnNames[r]
-                    # proxy = QGraphicsProxyWidget()
-                    # proxy.setWidget(QLabel(name, self))
-                    # self.__matrixLayout.addItem(proxy, row=r, col=c)
-                    # self.__matrixLayout.addWidget(QLabel(name, self), r, c, Qt.AlignCenter)
+                    name: str = names[r]
+                    self.__matrixLayout.addLabel(row=r, col=c, text=name)
                 else:
-                    xColName: str = attributesColumnNames[c]
-                    yColName: str = attributesColumnNames[r]
-                    plainSeriesList = self.__createScatterSeries(df=df, xCol=xColName, yCol=yColName,
-                                                                 groupBy=bool(groupName))
-                    # plainChart = ScatterPlotMatrix.__setupChartFromSeries(plainSeriesList,
-                    #                                                       xAxisName=xColName,
-                    #                                                       yAxisName=yColName)
-                    # invertedChart = ScatterPlotMatrix.__setupChartFromSeries(invertedSeriesList,
-                    #                                                          xAxisName=yColName,
-                    #                                                          yAxisName=xColName)
-                    # view1 = SimpleChartView(plainChart, self)
-                    # view2 = SimpleChartView(invertedChart, self)
-                    # view1.setRenderHint(QPainter.Antialiasing)
-                    # view2.setRenderHint(QPainter.Antialiasing)
-                    pc = self.__matrixLayout.addPlot(row=r, col=c)
-                    for series in plainSeriesList:
-                        pc.addItem(series)
-                    # # Inverted graph
-                    # ic = self.__matrixLayout.addPlot(row=c, col=r)
-                    #     pc.invertX()
-                    # self.__matrixLayout.addWidget(view2, c, r)
-        # self.__matrixLayout.setSpacing(2)
+                    xColName: str = names[c]
+                    yColName: str = names[r]
+                    seriesList = self.__createScatterSeries(df=df, xCol=xColName, yCol=yColName, groupBy=grouped,
+                                                            ds=self.__autoDownsample.isChecked())
+                    plot = self.__matrixLayout.addPlot(row=r, col=c)
+                    for series in seriesList:
+                        plot.addItem(series)
+        # When all plot are created stop spinner and re-enable button
+        self.spinner.stop()
+        self.__createButton.setEnabled(True)
 
-    def clearScatterPlotMatrix(self) -> None:
-        # if self.__matrixLayout:
-        #     # Delete every child widget
-        #     while self.__matrixLayout.sce
-        #         child = self.__matrixLayout.itemAt(0).widget()
-        #         if child:
-        #             self.__matrixLayout.removeWidget(child)
-        #             child.deleteLater()
-        #     self.__matrixLayout.deleteLater()
-        # Create a new widget for grid layout and add it to the splitter
-        # TODO: inspect for memory free
-        self.__matrixLayout.clear()
+    @staticmethod
+    def __createScatterSeries(df: Union[pd.DataFrame, pd.core.groupby.DataFrameGroupBy], xCol: str,
+                              yCol: str, groupBy: bool, ds: bool) -> List[pg.PlotDataItem]:
+        """
+        Creates a list of series of points to be plotted in the scatterplot
+
+        :param df: the input dataframe
+        :param xCol: name of the feature to use as x-axis
+        :param yCol: name of the feature to use as y-axis
+        :param groupBy: whether the feature dataframe is grouped by some attribute
+        :param ds: whether to auto downsample the set of points during rendering
+        :return:
+        """
+        allSeries = list()
+        if groupBy:
+            df: pd.core.groupby.DataFrameGroupBy
+            n = len(df)
+            colours = randomColors(n)
+            i = 0
+            for groupName, groupedDf in df:
+                # Remove any row with nan values
+                gdf = groupedDf.dropna()
+                qSeries1 = pg.PlotDataItem(x=gdf[xCol], y=gdf[yCol], autoDownsample=ds,
+                                           name=str(groupName), symbolBrush=pg.mkBrush(colours[i]),
+                                           symbol='o', pen=None)
+                allSeries.append(qSeries1)
+                i += 1
+        else:
+            df: pd.DataFrame
+            # Remove any row with nan values
+            df = df.dropna()
+            series = pg.PlotDataItem(x=df[xCol], y=df[yCol], autoDownsample=ds, symbol='o', pen=None)
+            allSeries.append(series)
+        return allSeries
 
     @Slot(str, str)
     def onFrameSelectionChanged(self, frameName: str, *_) -> None:
@@ -237,4 +222,34 @@ class ScatterPlotMatrix(DataView):
             oldModel.deleteLater()
         self.__colorByBox.setModel(self.__comboModel)
         # Reset attribute panel
-        self.clearScatterPlotMatrix()
+        self.resetScatterPlotMatrix()
+
+
+class ProcessDataframe:
+    def execute(self, filterDf: pd.DataFrame, group: int, attributes: List[int]) -> \
+            Tuple[pd.DataFrame, List[str], List[int], bool]:
+        """ Preprocess a dataframe for visualisation doing groupby operations if required """
+        groupName: Optional[str] = filterDf.columns[0] if group is not None else None
+        # Convert categories to numeric, but exclude groupBy attributes since categories are needed there
+        processed = self.__processCategoricalColumn(filterDf.iloc[:, 1:] if groupName else filterDf)
+        # Save attribute names for later use. Groupby column name is purposely excluded
+        attributesColumnNames: List[str] = processed.columns.to_list()
+        if groupName:
+            df = pd.concat([filterDf.loc[:, groupName], processed], axis=1, ignore_index=False)
+            # Group by selected attribute, if present
+            df = df.groupby(groupName)
+        else:
+            df = filterDf
+
+        return df, attributesColumnNames, attributes, bool(groupName)
+
+    @staticmethod
+    def __processCategoricalColumn(df: pd.DataFrame) -> pd.DataFrame:
+        """ For every categorical column in 'df' replace categories with their numerical code,
+        propagating Nan values. Categorical columns are converted to float """
+        categoricalColumns = df.select_dtypes(include='category').columns.to_list()
+        if categoricalColumns:
+            df = df.copy(True)
+            df[categoricalColumns] = \
+                df[categoricalColumns].apply(lambda c: c.cat.codes, axis=0).replace(-1, np.nan)
+        return df
